350f60108ba0b91271ee1f3d754696dda41e6c22
Siddhartha Basu <biosidd@gmail.com>
2014-09-01 18:58:47 +0100

    Added command for creating a private key file

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/cybersiddhu/go-micro-auth/service"
 
 	"gopkg.in/codegangsta/cli.v0"
 	"gopkg.in/yaml.v1"
@@ -27,17 +33,48 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 				}
-				svc := service.AuthService{}
+				svc := &service.AuthService{}
 				if err = svc.Run(config); err != nil {
 					log.Fatal(err)
 				}
-
 			},
+		},
+		{
+			Name:   "genkey",
+			Usage:  "Generate private key file",
+			Action: genPrivateKeyFile,
 		},
 	}
 	app.Run(os.Args)
 }
 
+// Generate a 2048 bit RSA private key and write to the file given
+// in the config file
+func genPrivateKeyFile(c *cli.Context) {
+	config, err := readConfig(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	out, err := os.Create(config.KeyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	prv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = pem.Encode(out, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(prv),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Reads the yaml config file from the path given in command line
 func readConfig(c *cli.Context) (service.Config, error) {
 	ypath := c.GlobalString("config")
 	config := service.Config{}
